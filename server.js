dd4415bb3cbb534a5ece1d6b212e34d16518b17b
alcobased <alcoholbasednontoxic@gmail.com>
2025-04-04 12:50:43 +0300

    ip

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const ipAdress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  res.send(`Hello from ${ipAdress}`);
 });
 
 app.listen(port, () => {
